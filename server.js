542a849569021bfad9bdd8154d51b88761ba4a2f
ABHISHEK SRIVASTAVA <32429040+gennopp@users.noreply.github.com>
2017-10-08 03:19:44 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+ var morgan = require('morgan');
+ var path = require('path');
+ 
+ var app = express();
+ app.use(morgan('combined'));
+ 
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ });
+ 
+ app.get('/ui/style.css', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+ });
+ 
+ app.get('/ui/madi.png', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  });
+  
+ var port = 80;
+ app.listen(port, function () {
+    console.log(`IMAD course app listening on port ${port}!`);
+  });
